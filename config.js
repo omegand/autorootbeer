8dd50e6f64325f721f70fe2fd501616ff3248287
Šarūnas <sarunyss@gmail.com>
2023-06-06 17:59:39 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,65 @@
+const settings = {
+    rootBeerCD: 55000,
+    enabled: true,
+    rootBeerIDs: [80081, 206045]
+};
+const skills = [
+    // BloodLust
+    {
+        group: 21,
+        job: 3,
+        delay: 5000
+    },
+    // Deadly Gamble
+    {
+        group: 20,
+        job: 0
+    },
+    // Mana Boost
+    {
+        group: 34,
+        job: 4
+    },
+    // Ragnarok
+    {
+        group: 12,
+        job: 12
+    },
+    // Godsfall
+    {
+        group: 25,
+        job: 12
+    },
+    // Adrenaline Rush
+    {
+        group: 17,
+        job: 1
+    },
+    // In Cold Blood
+    {
+        group: 20,
+        job: 2
+    },
+    // Windsong
+    {
+        group: 35,
+        job: 5
+    },
+    // Modular Weapon System
+    {
+        group: 41,
+        job: 9,
+        delay: 5000
+    },
+    // Inner Harmony
+    {
+        group: 23,
+        job: 11
+    },
+    //Rhythmic Blows
+    {
+        group: 26,
+        job: 10
+    }
+]
+module.exports = { settings, skills };
